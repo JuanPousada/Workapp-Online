7862a0c66875a0d872ac111afad4219f22ede91e
Juan Pousada <juanpousada99@gmail.com>
2024-07-14 10:53:36 -0300

    Modicacion para Login y Aspecto del Main

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,9 +8,8 @@ const connection = require("../bbdd")
 
 // SIGN UP
 
-exports.register =(req, res) =>{
-    console.log(req.body);
-
+exports.register = (req, res) => {
+  
     const nombre = req.body.nombreRegistro;
     const apellido = req.body.apellidoRegistro;
     const email = req.body.emailRegistro;
@@ -18,7 +17,7 @@ exports.register =(req, res) =>{
     const contrasenaC = req.body.contrasenaRegistroC;
     const pais = req.body.pais;
 
-    
+        
     
     connection.query('SELECT email FROM usuarios WHERE email= ?', [email], async(error, results) => {
         if (error) throw error;
@@ -33,7 +32,7 @@ exports.register =(req, res) =>{
         });
         }
         let hashedContrasena = await bcrypt.hash(contrasena, 8);
-        console.log(hashedContrasena)
+        
 
         connection.query('INSERT INTO usuarios SET ?', {nombre: nombre,apellido: apellido, email: email, contraseña: hashedContrasena}, (error, results)=>{
             if(error){
@@ -55,14 +54,45 @@ exports.register =(req, res) =>{
 }
 )}
 
+
+
 //LOGIN
 
 exports.login = (req, res)=>{
-    console.log('h')
-    console.log(req.body);
-    res.send('form')
-   /* const emailLogin = req.body.emailLogin;
+    const emailLogin = req.body.emailLogin;
     const contrasenaLogin = req.body.contrasenaLogin;
-    
-    console.log(emailLogin, contrasenaLogin);
-*/}
+
+    if (emailLogin !== '' & contrasenaLogin !== ''){
+        connection.query('SELECT email, contraseña FROM usuarios WHERE email= ?', [emailLogin], async(error, results) => {
+            if (error) throw error;
+            if (results.length > 0){
+                connection.query('SELECT contraseña FROM usuarios WHERE email= ?', [emailLogin], async(error, results) =>{
+                    if (error) throw error;
+                    let data =JSON.parse(JSON.stringify(results));
+                    let contraseñaDb = data[0].contraseña;
+                    console.log(contraseñaDb);
+                    bcrypt.compare(contrasenaLogin, contraseñaDb,(error, isMatch) =>{
+                        if(!isMatch){
+                            res.render('areaCliente',{
+                                messageLogin: 'Contraseña incorrecta'
+                            })
+                        }else{
+                            res.render('miCuenta')
+                        }
+                    }   
+                    )
+                })
+                
+            }else{
+                res.render('areaCliente',{
+                    messageLogin: 'No existe usuario registrado con ese mail'
+                })
+            }
+        })
+    }
+    else{
+        return res.render('areaCliente',{
+            messageLogin: 'Por favor complete todos los campos'
+        })
+    }    
+    }
